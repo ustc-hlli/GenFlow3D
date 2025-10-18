d68fc22558b1a0616f52171c5b2d6201986b6a3c
ustc-hlli <112604340+ustc-hlli@users.noreply.github.com>
2025-10-19 06:10:24 +0900

    Delete data/nuscenes/dl_np_tools directory

--- data/nuscenes/dl_np_tools/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# Copyright 2022 MBition GmbH
-# SPDX-License-Identifier: MIT
-
-
-__all__ = ["castf", "cast32", "cast64", "print_stats"]
-
-from .casting import cast32, cast64, castf
